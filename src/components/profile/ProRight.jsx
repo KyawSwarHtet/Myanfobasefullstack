4f262ab25ccbbaa233b55556cd833dd2d905daed
Mg Thung <thunghtike7@gmail.com>
2022-09-26 14:37:16 +0630

    profile

---
+++ src/components/profile/ProRight.jsx
@@ -0,0 +1,194 @@
+import React from "react";
+import Paginate from "../catepagerightbar/Paginate";
+export default function ProRight() {
+  return (
+    <>
+      <div>
+        <div className="lastnewbar-title">
+          <h2 className="capitalize">My Posts</h2>
+          <span className="lasttitleline Post-line"></span>
+        </div>
+        <div className="sidebar">
+          <div className="Trav-main">
+            <div className="postTrav">
+              <div className="Trav_img">
+                <img src="./images/technology/techhyperx.jpg" alt="" />
+                <p className="Travel1 cateTravel">Travel</p>
+              </div>
+              <div className="postTrav_info">
+                <h4>18 Top Fall Fashion Trends from New York Fashion</h4>
+                <p>
+                  All of the Best Looks From New York Fashion Week Fall/Winter
+                  2021.…
+                </p>
+                <div className="Probuttom">
+                  <div className="postman">
+                    <span className="DeleteP Delcolor">
+                      Delete <i className=" uil uil-trash-alt"></i>
+                    </span>
+                  </div>
+                  <div className="posticon">
+                    <span className="EditPost Editcolor">
+                      Edit <i className=" uil uil-edit"></i>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="postTrav">
+              <div className="Trav_img">
+                <img src="./images/technology/techhyperx.jpg" alt="" />
+                <p className="Travel1 cateTravel">Travel</p>
+              </div>
+              <div className="postTrav_info">
+                <h4>18 Top Fall Fashion Trends from New York Fashion</h4>
+                <p>
+                  All of the Best Looks From New York Fashion Week Fall/Winter
+                  2021.…
+                </p>
+                <div className="Probuttom">
+                  <div className="postman">
+                    <span className="DeleteP Delcolor">
+                      Delete <i className=" uil uil-trash-alt"></i>
+                    </span>
+                  </div>
+                  <div className="posticon">
+                    <span className="EditPost Editcolor">
+                      Edit <i className=" uil uil-edit"></i>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="postTrav">
+              <div className=" Trav_img">
+                <img src="./images/technology/techhyperx.jpg" alt="" />
+                <p className="Travel1 cateTravel">Travel</p>
+              </div>
+              <div className="postTrav_info">
+                <h4>18 Top Fall Fashion Trends from New York Fashion</h4>
+                <p>
+                  All of the Best Looks From New York Fashion Week Fall/Winter
+                  2021.…
+                </p>
+                <div className="Probuttom">
+                  <div className="postman">
+                    <span className="DeleteP Delcolor">
+                      Delete <i className=" uil uil-trash-alt"></i>
+                    </span>
+                  </div>
+                  <div className="posticon">
+                    <span className="EditPost Editcolor">
+                      Edit <i className=" uil uil-edit"></i>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="postTrav">
+              <div className="Trav_img">
+                <img src="./images/technology/techhyperx.jpg" alt="" />
+                <p className="Travel1 cateTravel">Travel</p>
+              </div>
+              <div className="postTrav_info">
+                <h4>18 Top Fall Fashion Trends from New York Fashion</h4>
+                <p>
+                  All of the Best Looks From New York Fashion Week Fall/Winter
+                  2021.…
+                </p>
+                <div className="Probuttom">
+                  <div className="postman">
+                    <span className="DeleteP Delcolor">
+                      Delete <i className=" uil uil-trash-alt"></i>
+                    </span>
+                  </div>
+                  <div className="posticon">
+                    <span className="EditPost Editcolor">
+                      Edit <i className=" uil uil-edit"></i>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="postTrav">
+              <div className="Trav_img">
+                <img src="./images/technology/techhyperx.jpg" alt="" />
+                <p className="Travel1 cateTravel">Travel</p>
+              </div>
+              <div className="postTrav_info">
+                <h4>18 Top Fall Fashion Trends from New York Fashion</h4>
+                <p>
+                  All of the Best Looks From New York Fashion Week Fall/Winter
+                  2021.…
+                </p>
+                <div className="Probuttom">
+                  <div className="postman">
+                    <span className="DeleteP Delcolor">
+                      Delete <i className=" uil uil-trash-alt"></i>
+                    </span>
+                  </div>
+                  <div className="posticon">
+                    <span className="EditPost Editcolor">
+                      Edit <i className=" uil uil-edit"></i>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="postTrav">
+              <div className="Trav_img">
+                <img src="./images/technology/techhyperx.jpg" alt="" />
+                <p className="Travel1 cateTravel">Travel</p>
+              </div>
+              <div className="postTrav_info">
+                <h4>18 Top Fall Fashion Trends from New York Fashion</h4>
+                <p>
+                  All of the Best Looks From New York Fashion Week Fall/Winter
+                  2021.…
+                </p>
+                <div className="Probuttom">
+                  <div className="postman">
+                    <span className="DeleteP Delcolor">
+                      Delete <i className=" uil uil-trash-alt"></i>
+                    </span>
+                  </div>
+                  <div className="posticon">
+                    <span className="EditPost Editcolor">
+                      Edit <i className=" uil uil-edit"></i>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="postTrav">
+              <div className="Trav_img">
+                <img src="./images/technology/techhyperx.jpg" alt="" />
+                <p className="Travel1 cateTravel">Travel</p>
+              </div>
+              <div className="postTrav_info">
+                <h4>18 Top Fall Fashion Trends from New York Fashion</h4>
+                <p>
+                  All of the Best Looks From New York Fashion Week Fall/Winter
+                  2021.…
+                </p>
+                <div className="Probuttom">
+                  <div className="postman">
+                    <span className="DeleteP Delcolor">
+                      Delete <i className=" uil uil-trash-alt"></i>
+                    </span>
+                  </div>
+                  <div className="posticon">
+                    <span className="EditPost Editcolor">
+                      Edit <i className=" uil uil-edit"></i>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <Paginate />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/profile/Savepost.jsx
@@ -0,0 +1,160 @@
+import React from "react";
+
+const Savepost = () => {
+  return (
+    <div>
+      <div className="lastnewbar-title">
+        <h2 className="capitalize">Save Posts</h2>
+        <span className="lasttitleline Post-line"></span>
+      </div>
+      <div className="lastnewbar-body sidebar1">
+        <div className="article-lastest">
+          <div className="lastnewimg">
+            <img src="./images/science/scienceinfo3.jpg" alt="" />
+          </div>
+          <div className="lastnewinfo">
+            <h4>Where does it come from effective from Exercise ?</h4>
+            <div className="lastnewbuttondate">
+              <span className="Catebutton2 cateTechnology">Technology</span>
+              <span className="DeleteS Delcolor">
+                <i class="uil uil-bookmark"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="article-lastest">
+          <div className="lastnewimg">
+            <img src="./images/science/scienceinfo1.jpg" alt="" />
+          </div>
+          <div className="lastnewinfo">
+            <h4>Where does it come from effective from Exercise ?</h4>
+            <div className="lastnewbuttondate">
+              <span className="Catebutton2 cateTechnology">Technology</span>
+              <span className="DeleteS Delcolor">
+                <i class="uil uil-bookmark"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="article-lastest">
+          <div className="lastnewimg">
+            <img src="./images/technology/techvideoimg.png" alt="" />
+          </div>
+          <div className="lastnewinfo">
+            <h4>Where does it come from effective from Exercise ?</h4>
+            <div className="lastnewbuttondate">
+              <span className="Catebutton2 cateTechnology">Technology</span>
+              <span className="DeleteS Delcolor">
+                <i class="uil uil-bookmark"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="article-lastest">
+          <div className="lastnewimg">
+            <img src="./images/science/scienceinfo4.jpg" alt="" />
+          </div>
+          <div className="lastnewinfo">
+            <h4>Where does it come from effective from Exercise ?</h4>
+            <div className="lastnewbuttondate">
+              <span className="Catebutton2 cateTechnology">Technology</span>
+              <span className="DeleteS Delcolor">
+                <i class="uil uil-bookmark"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="article-lastest">
+          <div className="lastnewimg">
+            <img src="./images/science/scienceinfo1.jpg" alt="" />
+          </div>
+          <div className="lastnewinfo">
+            <h4>Where does it come from effective from Exercise ?</h4>
+            <div className="lastnewbuttondate">
+              <span className="Catebutton2 cateTechnology">Technology</span>
+              <span className="DeleteS Delcolor">
+                <i class="uil uil-bookmark"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="article-lastest">
+          <div className="lastnewimg">
+            <img src="./images/science/scienceinfo5.jpg" alt="" />
+          </div>
+          <div className="lastnewinfo">
+            <h4>Where does it come from effective from Exercise ?</h4>
+            <div className="lastnewbuttondate">
+              <span className="Catebutton2 cateTechnology">Technology</span>
+              <span className="DeleteS Delcolor">
+                <i class="uil uil-bookmark"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="article-lastest">
+          <div className="lastnewimg">
+            <img src="./images/science/scienceinfo1.jpg" alt="" />
+          </div>
+          <div className="lastnewinfo">
+            <h4>Where does it come from effective from Exercise ?</h4>
+            <div className="lastnewbuttondate">
+              <span className="Catebutton2 cateTechnology">Technology</span>
+              <span className="DeleteS Delcolor">
+                <i class="uil uil-bookmark"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="article-lastest">
+          <div className="lastnewimg">
+            <img src="./images/science/scienceinfo1.jpg" alt="" />
+          </div>
+          <div className="lastnewinfo">
+            <h4>Where does it come from effective from Exercise ?</h4>
+            <div className="lastnewbuttondate">
+              <span className="Catebutton2 cateTechnology">Technology</span>
+              <span className="DeleteS Delcolor">
+                <i class="uil uil-bookmark"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="article-lastest">
+          <div className="lastnewimg">
+            <img src="./images/science/scienceinfo1.jpg" alt="" />
+          </div>
+          <div className="lastnewinfo">
+            <h4>Where does it come from effective from Exercise ?</h4>
+            <div className="lastnewbuttondate">
+              <span className="Catebutton2 cateTechnology">Technology</span>
+              <span className="DeleteS Delcolor">
+                <i class="uil uil-bookmark"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="article-lastest">
+          <div className="lastnewimg">
+            <img src="./images/science/scienceinfo1.jpg" alt="" />
+          </div>
+          <div className="lastnewinfo">
+            <h4>Where does it come from effective from Exercise ?</h4>
+            <div className="lastnewbuttondate">
+              <span className="Catebutton2 cateTechnology">Technology</span>
+              <span className="DeleteS Delcolor">
+                <i class="uil uil-bookmark"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Savepost;
